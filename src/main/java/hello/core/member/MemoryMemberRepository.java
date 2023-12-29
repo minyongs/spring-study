90f17ab15b5009129a86d490c9402b1e5633cba7
minyongs <stylish_c@naver.com>
2023-12-29 15:24:56 +0900

    ~17 강

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+    private static Map<Long, Member> store = new HashMap<>(); // 클래스에서만 관리
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member); //멤버에서 getId로 가져오고 , 옆은 멤버
+
+    }
+
+    @Override
+    public Member findByID(Long memberId) {
+        return store.get(memberId);
+    }
+}
